3d39cca53fe2182f8a4ceb2c0eb4713ab233f049
erichague76 <162602489+erichague76@users.noreply.github.com>
2026-06-10 08:05:06 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const PLATE_IMAGE_FILE = './Plate.png';
 const CONFIG = {
   plateTextColor: '#14377D',
   manualFontSize:1o0,
-  manualLetterSpacingPx: 0,
+  manualLetterSpacingPx: 20,
   manualOffsetX: 20,
-  manualOffsetY: 60,
+  manualOffsetY: 50,
   fontFamily: '"Arial Narrow", Bahnschrift, Arial, sans-serif',
   previewMaxWidthPx: 560,
 };
